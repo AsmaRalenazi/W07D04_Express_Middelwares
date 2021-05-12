d1a9bcb4c5218cffbb643f4208e0858969e6aa83
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-13 00:58:35 +0300

    practice q1 & solve q5 Pulse Check

--- main.js
+++ main.js
@@ -6,10 +6,18 @@ const port = 3000;
 //q4
 app.use(express.json());
 
+//q1 Practice
+const authRouter=express.Router();
 
 const users = ["John", "Mark"];
 
 app.get("/users", (req, res, next) => {
+    if(users.length === 0) {
+        const err = new Error("no users");
+  err.status = 500;
+  // pass it to next, we only pass values to `next` when we want to call the error handling middleware
+  next(err);
+    }
     res.json(users);
   });
 
@@ -22,7 +30,7 @@ const logUsers=(req,res,next)=>{
 }
 //q2
 //application-level middleware.
-app.use(logUsers );
+app.use(logUsers);
 
 //q3
 //Create a middleware function logMethod that logs the HTTP method 
@@ -34,7 +42,7 @@ next()
 }
 //the HTTP method 
 //and use it on the /users route.
-app.use("/users",logMethod)
+app.use("/users", logMethod)
 
 //q4
 //Use the right builtin middleware to parse JSON payloads from the request.
@@ -46,11 +54,53 @@ app.use("/users",logMethod)
 //the users array is empty,the middleware should have the message No users,
 // empty the array to test it out.
 
-app.get ("/users",(req,res,next)=>{
-const err=new Error ("No users")
-err.status=500;
-next()
-})
+// app.use ((err,req,res,next)=>{
+//     if(users === empty){
+//         next(err)
+//     }
+// })
+
+// app.use ((err,req,res,next)=>{
+// const err = new Error("No users")
+// err.status=500;
+// res.json(err)
+// })
+
+//Practice
+//q1
+//Create a new express router to handel all requests to /users,
+//.. and use it in the application, the endpoint /users should return all users.
+authRouter.get("/users", (req, res, next) => {
+    
+    // if(users.length === 0) {
+    //             const err = new Error("no users");
+    //       err.status = 500;
+    //       // pass it to next, we only pass values to `next` when we want to call the error handling middleware
+    //       next(err);
+    //         }
+            res.json(users);  });
+
+app.use(authRouter)
+
+
+
+
+
+
+
+  app.use((err, req, res, next) => {
+    // set the status code
+    res.status(err.status);
+    // send the response in JSON format
+    res.json({
+      error: {
+        status: err.status,
+        message: err.message,
+      },
+    });
+  });
+
+
 
 
 app.listen(port, () => {
