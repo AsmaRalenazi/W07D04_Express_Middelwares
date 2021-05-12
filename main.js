c9525e5bb5a58dda1686b001d43654fd7a6ff686
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-12 23:27:56 +0300

    start solve q1&q2 Pulse Check

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
+
 const express = require("express");
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+
 
 const users = ["John", "Mark"];
 
 app.get("/users", (req, res, next) => {
-  res.json(users);
-});
+    res.json(users);
+  });
+
+ //q1
+ //Create a middleware function logUsers that logs the users array then invokes the next middleware.
+const logUsers=(req,res,next)=>{
+    console.log(users)
+    next()
+}
+//q2
+//application-level middleware.
+app.use(fun0);
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
